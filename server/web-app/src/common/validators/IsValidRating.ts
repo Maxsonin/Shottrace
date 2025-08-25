1f5689d7b206b3f7fe2496d14d1331d7f1eac783
Maxsonin <maximles.lv@gmail.com>
2025-08-25 11:33:23 +0300

    feat(reviews): allow 0.5 rating

--- server/web-app/src/common/validators/IsValidRating.ts
+++ server/web-app/src/common/validators/IsValidRating.ts
@@ -13,14 +13,14 @@ export function IsValidRating(validationOptions?: ValidationOptions) {
       options: validationOptions,
       validator: {
         validate(value: any, _args: ValidationArguments) {
-          const allowed = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0];
+          const allowed = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0];
           return (
             typeof value === 'number' &&
             allowed.includes(Number(value.toFixed(1)))
           );
         },
         defaultMessage: () =>
-          'Rating must be one of the following values: 1.0, 1.5, ..., 5.0',
+          'Rating must be one of the following values: 0.5, 1.0, 1.5, ..., 5.0',
       },
     });
   };
