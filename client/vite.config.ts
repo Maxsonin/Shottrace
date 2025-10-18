b668aeec5941a89ef930b7f5ba4b107980c696a9
Maxsonin <maximles.lv@gmail.com>
2025-10-18 20:33:52 +0300

    feature(config): configure build and vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
+import viteCompression from 'vite-plugin-compression';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    visualizer({
+      filename: './reports/stats.html',
+      open: true,
+    }) as PluginOption,
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: false,
   },
 });
